30f9718fb26d7734deea6e3f68b4f21cdebea85c
cowchimp <cowchimp@users.noreply.github.com>
2016-06-26 21:29:47 +0300

    fix double `SYW.SYW`

--- proxy.js
+++ proxy.js
@@ -1,28 +1,26 @@
 var Hashes = require('jshashes');
 var request = require('request');
 
-exports.SYW = function (appId, appSecret) {
-  function callEndpoint(endpoint, query, token, callback) {
-    query = query || {};
-    query.token = token;
-    query.hash = generateAuthHash(token);
-    var uri = 'https://platform.shopyourway.com' + endpoint;
+exports.callEndpoint = function(appId, appSecret, endpoint, query, token, callback) {
+  query = query || {};
+  query.token = token;
+  query.hash = generateAuthHash(token);
+  var uri = 'https://platform.shopyourway.com' + endpoint;
 
-    var options = {
-      uri: uri,
-      qs: query,
-      json: true
-    };
+  var options = {
+    uri: uri,
+    qs: query,
+    json: true
+  };
 
-    request(options, function(err, response, body) {
-      if(err) {
-        var errMsg = err && err.message ? ' : ' + err.message : '';
-        callback(new Error('Error while trying to call platform endpoint ' + endpoint + errMsg));
-        return;
-      }
-      callback(null, body);
-    });
-  }
+  request(options, function (err, response, body) {
+    if (err) {
+      var errMsg = err && err.message ? ' : ' + err.message : '';
+      callback(new Error('Error while trying to call platform endpoint ' + endpoint + errMsg));
+      return;
+    }
+    callback(null, body);
+  });
 
   function generateAuthHash(token) {
     return generateHash(token + appSecret);
@@ -31,8 +29,4 @@ exports.SYW = function (appId, appSecret) {
   function generateHash(str) {
     return new Hashes.SHA256().hex(str);
   }
-
-  return {
-    callEndpoint: callEndpoint
-  };
 };
